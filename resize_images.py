239d2a4267083ca5d684eb25e906cb64610d203a
MaggieThomann <mmthomann@gmail.com>
2018-05-02 01:05:01 -0400

    Maggie changes to resize image

--- resize_images.py
+++ resize_images.py
@@ -10,23 +10,29 @@ import sys
 import os
 
 image_dir = "/Users/mariainesaranguren/Desktop/ML_luigi/Images"
+image_dir_maggie = "/Volumes/LaCie/School/Machine Learning/FinalProject/Luigi_code/Images"
 NEW_WIDTH = 256
 NEW_HEIGHT = 256
 
 # Traverse root directory, and list directories as dirs and files as files
-for root, dirs, files in os.walk(image_dir):
+for root, dirs, files in os.walk(image_dir_maggie):
+	# Ignore hidden files
+	files = [f for f in files if not f[0] == '.']
+    	dirs[:] = [d for d in dirs if not d[0] == '.']
+
+    	# Continue program
 	path = root.split(os.sep)
 	print("Resizing directory", os.path.basename(root))
 	for image_file in files:
 	# Go through each image in the list and resize it
-		print ("Resizing image", image_dir+"/"+os.path.basename(root)+"/"+image_file)
+		print ("Resizing image", image_dir_maggie+"/"+os.path.basename(root)+"/"+image_file)
 		# Read the images and landmark ids into the dictionary
-		with open(image_dir+"/"+os.path.basename(root)+"/"+image_file, "rb") as f:
+		with open(image_dir_maggie+"/"+os.path.basename(root)+"/"+image_file, "rb") as f:
 			try:
 				# Resize the image
-				img = Image.open(image_dir+"/"+os.path.basename(root)+"/"+image_file)
+				img = Image.open(image_dir_maggie+"/"+os.path.basename(root)+"/"+image_file)
 				img_resized = resizeimage.resize_cover(img, [NEW_WIDTH, NEW_HEIGHT])
-				img_resized.save(image_dir+"/"+os.path.basename(root)+"/"+image_file)
+				img_resized.save(image_dir_maggie+"/"+os.path.basename(root)+"/"+image_file)
 			except:
 				print ("Error")
 				pass
