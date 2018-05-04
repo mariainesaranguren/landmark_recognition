3baa6043e684b02efc043071055465a7eb34a8c4
Maria Ines Aranguren <marangu1@nd.edu>
2018-05-04 11:24:02 -0400

    Added metrics and got rid of irrelevant ones

--- Results/analyze_results.py
+++ Results/analyze_results.py
@@ -1,9 +1,14 @@
 ''' Analyzes results from csv files created from newtest2.py '''
 
 import sys
+import pandas as pd
 
 def get_average(l):
     return (sum(l)/len(l))
+
+def get_statistics(l):
+    s = pd.Series(l)
+    return s.describe()
 
 if len(sys.argv) != 2 or sys.argv[1] == "-h":
     print ("Usage: python analyze_predictions.py results.csv")
@@ -19,7 +24,7 @@ all_confidence = []
 all_rank = []
 f.close()
 for img in results:
-    img = img[:-2]
+    img = img[:-2]                      # Strip new line character
     img_result = img.split(",")
     landmark_id = img_result[0]
     img_id = img_result[1].split(".")[0]
@@ -44,17 +49,24 @@ for landmark_id in landmark_confidence.keys():
     average_landmark_rank.append(get_average(landmark_rank[landmark_id]))
 
 # Total number of predictions
+print ("*** General ***")
 print ("Total number of predictions made:\t", len(all_rank))
+print ("Total number of landmarks involved:\t", len(landmark_confidence.keys()))
 
 # CONFIDENCE
 # Confidence is 0 if it is not in one of the top five guesses
-print ("\nConfidence: ")
+print ("\n*** Confidence ***")
 weighted_score = sum(all_confidence)
 print ("Summation of all confidences:\t\t", weighted_score)
-print ("Overall average landmark confidence:\t", get_average(average_landmark_confidence))
-print ("*Perfect identification with complete confidence would give summation of", len(all_rank))
-print ("*This is skewed because there is no penalization for incorrect predictions")
-
+confidence_spread = get_statistics(average_landmark_confidence)
+print ("Spread of confidence results:")
+print ("\tMin:", confidence_spread['min'])
+print ("\tStd:", confidence_spread['std'])
+print ("\t25%:", confidence_spread['25%'])
+print ("\t50%:", confidence_spread['50%'])
+print ("\t75%:", confidence_spread['75%'])
+print ("\tMax:", confidence_spread['max'])
+print ("* Note that a confidence of 0 is given to images that are not identified with 5 guesses.")
 
 
 # RANK
@@ -65,16 +77,14 @@ for rank in all_rank:
         count_rank[rank] += 1
     else:
         count_rank[rank] = 1
-print ("\nRank: ")
+print ("\n*** Rank ***")
 print ("Count of each rank level:\t\t", count_rank)
 print ("P(Not Identified):\t\t\t", count_rank[0]/len(all_rank))
 for n in range(1, 6):
     identified = 0
     for i in range(1,n+1):
         identified += count_rank[i]
-    print ("P(Identified landmark in top", n, "guesse(s)):", identified/len(all_rank))
-
-
+    print ("P(Identified landmark in top", n, "guesses):", identified/len(all_rank))
 
 
 
